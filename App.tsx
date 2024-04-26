1f1b97a041923b09aaff8b8a0f602d588f3331c3
Josemar Silvestre <josimarsilvestre16@gmail.com>
2024-04-26 20:53:07 +0100

    modified:   App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, LogBox } from 'react-native';
 import { ApolloProvider } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Provider } from './src/context/authContext';
 import client from './src/api/client';
 import MainStackNavigator from './components/MyStack';
+
+LogBox.ignoreLogs(["Sending `onAnimatedValueUpdate` with no listeners registered."]);
 
 export default function App() {
     return (
