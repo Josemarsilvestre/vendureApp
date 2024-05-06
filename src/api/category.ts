6c64a946e2d1f39d4baa8a3f25172559324e4725
Josemar Silvestre <josimarsilvestre16@gmail.com>
2024-05-06 18:17:26 +0100

    modified:   src/api/category.ts

--- src/api/category.ts
+++ src/api/category.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_ALL_COLLECTIONS_QUERY = gql`
   query GetAllCollections {
-    collections(options: { take: 8 }) {
+    collections(options: { take: 9 }) {
       items {
         id
         name
